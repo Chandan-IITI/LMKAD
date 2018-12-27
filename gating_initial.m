b9865630bd1c8acaba10440a51df4eff72bb8be6
Chandan Gautam <chandangautam31@gmail.com>
2018-12-27 17:26:58 +0530

    Update gating_initial.m

--- gating_initial.m
+++ gating_initial.m
@@ -1,3 +1,11 @@
+% Reference
+%   gonen08icml
+%   Localized Multiple Kernel Learning
+%   Mehmet Gonen, Ethem Alpaydin
+% Mehmet Gonen (gonen@boun.edu.tr)
+% Department of Computer Engineering, Bogazici University
+%   Proceedings of the 25th International Conference on Machine Learning, 2008
+
 function gat = gating_initial(loc, P, typ)
     N = size(loc, 1);
     DG = size(loc, 2);
