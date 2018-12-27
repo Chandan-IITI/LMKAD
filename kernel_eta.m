966cdd2ded3c4b2503305a435f7ee269d86f36d4
Chandan Gautam <chandangautam31@gmail.com>
2018-12-27 17:27:52 +0530

    Update kernel_eta.m

--- kernel_eta.m
+++ kernel_eta.m
@@ -1,3 +1,11 @@
+% Reference
+%   gonen08icml
+%   Localized Multiple Kernel Learning
+%   Mehmet Gonen, Ethem Alpaydin
+% Mehmet Gonen (gonen@boun.edu.tr)
+% Department of Computer Engineering, Bogazici University
+%   Proceedings of the 25th International Conference on Machine Learning, 2008
+
 function Keta = kernel_eta(Km, eta)
     N = size(Km, 1);
     P = size(Km, 3);
