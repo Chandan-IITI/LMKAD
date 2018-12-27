2465b9aaa89c35fa6584d5c8914302df190b7254
Chandan Gautam <chandangautam31@gmail.com>
2018-12-27 17:27:28 +0530

    Update get_kernel.m

--- get_kernel.m
+++ get_kernel.m
@@ -1,3 +1,11 @@
+% Reference
+%   gonen08icml
+%   Localized Multiple Kernel Learning
+%   Mehmet Gonen, Ethem Alpaydin
+% Mehmet Gonen (gonen@boun.edu.tr)
+% Department of Computer Engineering, Bogazici University
+%   Proceedings of the 25th International Conference on Machine Learning, 2008
+
 function ker = get_kernel(nam)
     sub = strfind(nam, '@');
     if isempty(sub) == 1
